981a8149b5e83e9364b5e113821416ef8b2dfe18
Alx31 <alex31.couderc@gmail.com>
2019-01-20 15:22:16 +0100

    take this away we got it in vendor now

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -131,10 +131,6 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(COMMON_PATH)/sepolicy/private
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/qcom/sepolicy/private
 BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/qcom/sepolicy/public
 
-BOARD_SEPOLICY_DIRS += \
-    device/xiaomi/beryllium/sepolicy
-SELINUX_IGNORE_NEVERALLOWS := true
-
 # Treble
 BOARD_VNDK_RUNTIME_DISABLE := true
 BOARD_VNDK_VERSION := current
